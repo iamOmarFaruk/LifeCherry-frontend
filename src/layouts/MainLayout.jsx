b49528b1d90007da0e74c1cb5e93a1ced9c4e93f
Omar Faruk <omarbg.bd@gmail.com>
2025-12-06 00:54:15 +0600

    fix: add ScrollToTop to reset scroll position on route change

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/shared/Navbar';
 import Footer from '../components/shared/Footer';
+import ScrollToTop from '../components/shared/ScrollToTop';
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navbar />
       {/* Spacer for fixed navbar */}
       <div className="h-16"></div>
